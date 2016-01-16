038749df509cce6d59fa44e65806cbe5097b3442
desc0n <desconbk@yandex.ru>
2016-01-16 16:49:41 +1000

    Корректирую стиль меню

--- application/views/index.php
+++ application/views/index.php
@@ -1,9 +1,7 @@
 <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 text-center">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 menu">
         <?foreach ($pages as $page) {?>
-        <div class="menu-item">
-            <a href="/page/<?=$page['slug'];?>"><?=$page['title'];?></a>
-        </div>
+        <a class="menu-item" href="/page/<?=$page['slug'];?>"><?=$page['title'];?></a>
         <?}?>
     </div>
 </div>
