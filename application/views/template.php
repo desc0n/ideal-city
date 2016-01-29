7986b9bb67647677890c58d282bfe34ecdfae193
desc0n <desconbk@yandex.ru>
2016-01-29 23:46:41 +1000

    Меняю footer

--- application/views/template.php
+++ application/views/template.php
@@ -29,7 +29,6 @@
     <script type="text/javascript">
         $(document).ready(function(){
             $('.hit, .page').rollbar({zIndex:80});
-            $('body').rollbar({zIndex:100});
         });
     </script>
     <script type="text/javascript">
@@ -49,90 +48,94 @@
     </script>
 </head>
 <body>
-    <div class="header">
-        <div class="container">
-            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 no-padding text-center">
-                <a href="/" target="_self"><img src="/public/i/logo.png" class="logo"></a>
-            </div>
-            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 header-container">
-                <div class="header-top">П Р О Е К Т Н О   -   Д Е В Е Л О П Е Р С К А Я    Г Р У П П А</div>
-                <h1 class="header-title">ИДЕАЛЬНЫЙ ГОРОД</h1>
-                <div class="header-links">
-                    <a href="/scope/1">ДЕВЕЛОПМЕНТ</a>  <span class="header-point"> &#149; </span>
-                    <a href="/scope/2">ПРОЕКТИРОВАНИЕ</a> <span class="header-point"> &#149; </span>
-                    <a href="/scope/3">КАДАСТР НЕДВИЖИМОСТИ</a> <span class="header-point"> &#149; </span>
-                    <a href="/scope/4">ИЗЫСКАНИЕ</a>
+    <div id="wrapper">
+        <div class="header">
+            <div class="container">
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 no-padding text-center">
+                    <a href="/" target="_self"><img src="/public/i/logo.png" class="logo"></a>
                 </div>
-                <div class="header-contacts">
-                    <p>690091 Приморский край, г. Владивосток, ул. Луцкого 16, 1 эт.</p>
-                    <p>тел.: +7(423) 220-99-54; +7(423) 266-66-15</p>
-                    <p>office@ideal-gorod.ru</p>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 header-container">
+                    <div class="header-top">П Р О Е К Т Н О   -   Д Е В Е Л О П Е Р С К А Я    Г Р У П П А</div>
+                    <h1 class="header-title">ИДЕАЛЬНЫЙ ГОРОД</h1>
+                    <div class="header-links">
+                        <a href="/scope/1">ДЕВЕЛОПМЕНТ</a>  <span class="header-point"> &#149; </span>
+                        <a href="/scope/2">ПРОЕКТИРОВАНИЕ</a> <span class="header-point"> &#149; </span>
+                        <a href="/scope/3">КАДАСТР НЕДВИЖИМОСТИ</a> <span class="header-point"> &#149; </span>
+                        <a href="/scope/4">ИЗЫСКАНИЕ</a>
+                    </div>
+                    <div class="header-contacts">
+                        <p>690091 Приморский край, г. Владивосток, ул. Луцкого 16, 1 эт.</p>
+                        <p>тел.: +7(423) 220-99-54; +7(423) 266-66-15</p>
+                        <p>office@ideal-gorod.ru</p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container content">
-        <?=$content;?>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 news">
-            <div class="news-title">Новости</div>
-            <?foreach ($news as $newsData) {?>
-            <div class="news-date"><?=Date::convertStrDateToFormat($newsData['date'], 'd.m.Y');?> г.</div>
-            <div class="news-content">
-                <?=mb_substr($newsData['content'], 0, 100);?>...
-                <a href="#" class="news-link">Подробнее</a>
+        <div class="container content">
+            <?=$content;?>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 news">
+                <div class="news-title">Новости</div>
+                <?foreach ($news as $newsData) {?>
+                    <div class="news-date"><?=Date::convertStrDateToFormat($newsData['date'], 'd.m.Y');?> г.</div>
+                    <div class="news-content">
+                        <?=mb_substr($newsData['content'], 0, 100);?>...
+                        <a href="#" class="news-link">Подробнее</a>
+                    </div>
+                <?}?>
+                <table style="border-collapse: collapse; height: 25%; width: 25%;">
+                    <tbody>
+                    <tr>
+                        <td style="border: 0 solid rgb(0, 0, 0);">
+                            <script type="text/javascript" src="/tagcloud/swfobject.js"></script>
+                            <div id="3dcloud_block" style="text-align:center;font-size:12pt;">
+                                Для отображения блока требуется
+                                <noindex>
+                                    <a href="http://get.adobe.com/flashplayer" rel="nofollow" target="_blank" title="Скачать Flash Player бесплатно">
+                                        Flash Player
+                                    </a>
+                                </noindex>
+                                <br><br>
+                                $MYINF_0$
+                            </div>
+                            <script type="text/javascript">
+                                var rnumber = Math.floor(Math.random()*9999999);
+                                var so = new SWFObject("/tagcloud/tagcloud.swf?r="+rnumber, "tagcloudflash", "185", "185", "9");
+                                var tags = "" +
+                                    <?foreach ($cloudTag as $tag) {?>
+                                    "<a href='/page/<?=$tag['slug'];?>' style='font-size:8px;'><?=$tag['title'];?></a>" +
+                                    <?}?>
+                                    "";
+                                tags = tags.replace(/"/g,"‘");
+                                tags = tags.replace(/<noindex>/gi,"");
+                                tags = tags.replace(/<\/noindex>/gi,"");
+                                tags = "<tags>" + tags + "</tags>";
+                                so.addParam("wmode", "transparent");
+                                so.addParam("allowScriptAccess", "always");
+                                so.addVariable("minFontSize", "1");
+                                so.addVariable("maxFontSize", "2.5");
+                                so.addVariable("tcolor", "0xE31E24");
+                                so.addVariable("tcolor2", "0xE31E24");
+                                so.addVariable("hicolor", "0xE31E24");
+                                so.addVariable("tspeed", "100");
+                                so.addVariable("distr", "true");
+                                so.addVariable("mode", "tags");
+                                so.addVariable("tagcloud", tags);
+                                so.write("3dcloud_block");
+                            </script>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
-            <?}?>
-            <table style="border-collapse: collapse; height: 25%; width: 25%;">
-                <tbody>
-                <tr>
-                    <td style="border: 0 solid rgb(0, 0, 0);">
-                        <script type="text/javascript" src="/tagcloud/swfobject.js"></script>
-                        <div id="3dcloud_block" style="text-align:center;font-size:12pt;">
-                            Для отображения блока требуется
-                            <noindex>
-                                <a href="http://get.adobe.com/flashplayer" rel="nofollow" target="_blank" title="Скачать Flash Player бесплатно">
-                                    Flash Player
-                                </a>
-                            </noindex>
-                            <br><br>
-                            $MYINF_0$
-                        </div>
-                        <script type="text/javascript">
-                            var rnumber = Math.floor(Math.random()*9999999);
-                            var so = new SWFObject("/tagcloud/tagcloud.swf?r="+rnumber, "tagcloudflash", "185", "185", "9");
-                            var tags = "" +
-                            <?foreach ($cloudTag as $tag) {?>
-                            "<a href='/page/<?=$tag['slug'];?>' style='font-size:8px;'><?=$tag['title'];?></a>" +
-                            <?}?>
-                            "";
-                            tags = tags.replace(/"/g,"‘");
-                            tags = tags.replace(/<noindex>/gi,"");
-                            tags = tags.replace(/<\/noindex>/gi,"");
-                            tags = "<tags>" + tags + "</tags>";
-                            so.addParam("wmode", "transparent");
-                            so.addParam("allowScriptAccess", "always");
-                            so.addVariable("minFontSize", "1");
-                            so.addVariable("maxFontSize", "2.5");
-                            so.addVariable("tcolor", "0xE31E24");
-                            so.addVariable("tcolor2", "0xE31E24");
-                            so.addVariable("hicolor", "0xE31E24");
-                            so.addVariable("tspeed", "100");
-                            so.addVariable("distr", "true");
-                            so.addVariable("mode", "tags");
-                            so.addVariable("tagcloud", tags);
-                            so.write("3dcloud_block");
-                        </script>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
         </div>
-    </div>
-    <div class="container footer">
-        <?=$footer;?>
+        <div class="container container-bottom">
+             <div class="footer">
+                <?=$footer;?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
