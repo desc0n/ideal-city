583b6f3e21293c290a6fcd9b24a347600ba9c4bd
desc0n <desconbk@yandex.ru>
2016-02-16 20:29:26 +1000

    Скрываю меню, при разворачивании другого меню.

--- application/views/menu.php
+++ application/views/menu.php
@@ -5,32 +5,36 @@
         if ($page['slug'] == 'portfolio') {
             $collapseIn = $request->action() == 'portfolio' ? 'in' : '';
             ?>
-    <div class="menu-item"  id="portfolio">
-        <a data-toggle="collapse" data-parent="#portfolio" href="#collapseOne">
-            Портфолио
-        </a>
-    </div>
-    <div class="portfolio-menu collapse <?=$collapseIn;?>"  id="collapseOne">
+    <div class="panel">
+        <div class="menu-item"  id="portfolio">
+            <a data-toggle="collapse" href="#collapseOne"  data-parent=".menu">
+                Портфолио
+            </a>
+        </div>
+        <div class="portfolio-menu collapse <?=$collapseIn;?>"  id="collapseOne">
         <?foreach ($portfolioPages as $portfolioPage) {
             $portfolioClass = $portfolioPage['id'] == $request->param('id') && $request->action() == 'portfolio'? 'portfolio-menu-item active' : 'portfolio-menu-item';
             ?>
             <a class="<?=$portfolioClass;?>" href="/portfolio/<?=$portfolioPage['id'];?>"><?=$portfolioPage['title'];?></a>
         <?}?>
+        </div>
     </div>
         <?} elseif ($page['slug'] == 'scope') {
             $collapseIn = $request->action() == 'scope' ? 'in' : '';
             ?>
-    <div class="menu-item"  id="scope">
-        <a data-toggle="collapse" data-parent="#scope" href="#collapseTwo">
-            Сфера деятельности
-        </a>
-    </div>
-    <div class="scope-menu collapse <?=$collapseIn;?>"  id="collapseTwo">
+    <div class="panel">
+        <div class="menu-item"  id="scope">
+            <a data-toggle="collapse" href="#collapseTwo" data-parent=".menu">
+                Сфера деятельности
+            </a>
+        </div>
+        <div class="scope-menu collapse <?=$collapseIn;?>"  id="collapseTwo">
         <?foreach ($scopePages as $scopePage) {
             $scopeClass = $scopePage['id'] == $request->param('id') && $request->action() == 'scope'? 'scope-menu-item active' : 'scope-menu-item';
             ?>
             <a class="<?=$scopeClass;?>" href="/scope/<?=$scopePage['id'];?>"><?=$scopePage['title'];?></a>
         <?}?>
+        </div>
     </div>
         <?} else {
             $class = $page['slug'] == $request->param('id') ? 'menu-item active' : 'menu-item';
@@ -39,4 +43,3 @@
             <?
         }
     }?>
-
