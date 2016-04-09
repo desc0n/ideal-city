aebffcb76c07a1824293639cc126f568edf3c34c
desc0n <desconbk@yandex.ru>
2016-04-09 15:56:18 +1000

    Блокирую прокрутку страницы

--- application/views/page.php
+++ application/views/page.php
@@ -17,5 +17,25 @@
 <script>
     window.onload = function() {
         baguetteBox.run('.baguetteBoxOne');
+
+        document.onmousewheel = document.onwheel = function()
+        {
+            return false;
+        };
+
+        document.addEventListener(
+            "MozMousePixelScroll",function(){
+                return false
+            },
+            false
+        );
+
+        document.onkeydown=function(e) {
+            if (e.keyCode>=33&&e.keyCode<=40) {
+                return false;
+            }
+        }
     };
+
+
 </script>
